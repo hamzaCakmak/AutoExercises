e480f9b30aab6e279711a3ed3df63879db864d37
hamzaCakmak <40656986+hamzaCakmak@users.noreply.github.com>
2019-05-06 14:04:28 +0300

    Add files via upload

--- Project/sketch.js
+++ Project/sketch.js
@@ -4,16 +4,16 @@ var rule1 = {
 var rule2 = {
 	  "S": ["#NP# #VP#"],
 	  "NP": ["the #N#"],
-	  "VP": ["#V#", "#V# #NP#"],
+	  "VP": ["#V#", "#V# by #NP#"],
 	  "N": ["bear","lion", "rabbit", "wolf"],
-	  "V": ["beaten","haunted","scared", "escaped", "escaped"]
+	  "V": ["beaten","haunted","scared", "captured", "killed"]
 }
 var rule3 = {
 	"name": ["Aragorn","Sam","Legolas","Mithrandir","Faramir","Frodo","Gimli"],
-	"creatures": ["dragon","warg","great spider","great eagle","werewolf","giant","troll","fellbeast"],
+	"creature": ["dragon","warg","great spider","great eagle","werewolf","giant","troll","fellbeast"],
 	"mood": ["vexed","indignant","impassioned","wistful","astute","courteous","frightened"],
-	"story": ["#hero# traveled with his #heroPet#.  #hero# was never #mood#, for the #heroPet# was always too #mood#."],
-	"origin": ["#[hero:#name#][heroPet:#creatures#]story#"]
+	"story": ["#hero# traveled with his pet #heroPet#.  #hero# was never #mood#, for the #heroPet# was always too #mood#."],
+	"origin": ["#[hero:#name#][heroPet:#creature#]story#"]
 }
 var grammar;
 
